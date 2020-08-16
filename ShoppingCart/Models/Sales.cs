7c0a05b8c0f0b94c086c239dad7cf34045eaa6fa
Melisha <mslafaber1@gmail.com>
2020-08-16 22:27:08 +0930

    Created the MVC Application with individual authentication and created models for items.cs and Sales.cs

---
+++ ShoppingCart/Models/Sales.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models
+{
+    public class Sales
+    {
+        public int Id { get; set; }
+        public int Item { get; set; }
+        public string Buyer { get; set; }
+        public int Quantity { get; set; }
+    }
+}
